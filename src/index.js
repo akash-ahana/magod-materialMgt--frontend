a2ec25e306d8437d0b55d0fa884bb0b7cff094f6
nilesh-ahana <nilesh.hanumant@ahana.co.in>
2023-02-23 15:59:52 +0530

    changes in part new
    changes in part new

--- src/index.js
+++ src/index.js
@@ -4,15 +4,15 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "react-confirm-alert/src/react-confirm-alert.css";
 // import { Provider } from "react-redux";
 // import store from "./store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <Provider store={store}>
-    <App />
+  <App />
   // </Provider>
-
 );
 
 // If you want to start measuring performance in your app, pass a function
